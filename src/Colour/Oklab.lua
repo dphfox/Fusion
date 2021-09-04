5c38763b27ccfa048d34dc57c2850590c97edee7
Elttob <dan.bottlefact@gmail.com>
2021-09-04 18:37:43 +0100

    Fix spaces -> tabs

--- src/Colour/Oklab.lua
+++ src/Colour/Oklab.lua
@@ -13,14 +13,14 @@ function Oklab.to(rgb: Color3): Vector3
 	local m = rgb.R * 0.2119034982 + rgb.G * 0.6806995451 + rgb.B * 0.1073969566
 	local s = rgb.R * 0.0883024619 + rgb.G * 0.2817188376 + rgb.B * 0.6299787005
 
-    local lRoot = l ^ (1/3)
-    local mRoot = m ^ (1/3)
-    local sRoot = s ^ (1/3)
+	local lRoot = l ^ (1/3)
+	local mRoot = m ^ (1/3)
+	local sRoot = s ^ (1/3)
 
-    return Vector3.new(
-        lRoot * 0.2104542553 + mRoot * 0.7936177850 - sRoot * 0.0040720468,
-        lRoot * 1.9779984951 - mRoot * 2.4285922050 + sRoot * 0.4505937099,
-        lRoot * 0.0259040371 + mRoot * 0.7827717662 - sRoot * 0.8086757660
+	return Vector3.new(
+		lRoot * 0.2104542553 + mRoot * 0.7936177850 - sRoot * 0.0040720468,
+		lRoot * 1.9779984951 - mRoot * 2.4285922050 + sRoot * 0.4505937099,
+		lRoot * 0.0259040371 + mRoot * 0.7827717662 - sRoot * 0.8086757660
 	)
 end
 
@@ -28,12 +28,12 @@ end
 -- The Color3 will be clamped by default unless specified otherwise.
 function Oklab.from(lab: Vector3, unclamped: boolean?): Color3
 	local lRoot = lab.X + lab.Y * 0.3963377774 + lab.Z * 0.2158037573
-    local mRoot = lab.X - lab.Y * 0.1055613458 - lab.Z * 0.0638541728
-    local sRoot = lab.X - lab.Y * 0.0894841775 - lab.Z * 1.2914855480
+	local mRoot = lab.X - lab.Y * 0.1055613458 - lab.Z * 0.0638541728
+	local sRoot = lab.X - lab.Y * 0.0894841775 - lab.Z * 1.2914855480
 
-    local l = lRoot ^ 3
-    local m = mRoot ^ 3
-    local s = sRoot ^ 3
+	local l = lRoot ^ 3
+	local m = mRoot ^ 3
+	local s = sRoot ^ 3
 
 	local red = l * 4.0767416621 - m * 3.3077115913 + s * 0.2309699292
 	local green = l * -1.2684380046 + m * 2.6097574011 - s * 0.3413193965
