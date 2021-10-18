0678eb01aab4dca9b0b6ea754019842124ed9d38
Elttob <dan.bottlefact@gmail.com>
2021-10-18 02:46:28 +0100

    Correctness pass on Types.lua

--- src/Types.lua
+++ src/Types.lua
@@ -2,46 +2,28 @@
 	Stores Luau type definitions shared across scripts in Fusion.
 ]]
 
+--[[
+	General use types
+]]
+
+-- A set collection - keys are the elements of the set. Values are ignored.
 export type Set<T> = {[T]: any}
 
-export type State<T> = {
-  get: (State<T>, asDependency: boolean?) -> T,
-  set: (State<T>, newValue: any, force: boolean?) -> ()
-}
-
-export type StateOrValue<T> = State<T> | T
-
-export type Computed<T> = {
-  get: (Computed<T>, asDependency: boolean?) -> T,
-  update: (Computed<T>) -> boolean
-}
-
-export type Compat = {
-  update: (Compat) -> boolean,
-  onChange: (Compat, callback: () -> ()) -> ()
-}
-
+-- A unique symbolic value - can also store a key for variants.
 export type Symbol = {
-	type: string,
+	type: string, -- replace with "Symbol" when Luau supports singleton types
 	name: string,
 	key: string?
 }
 
+-- Stores useful information about Luau errors.
 export type Error = {
 	raw: string,
 	message: string,
 	trace: string
 }
 
-export type Dependency<T> = State<T> & {
-	dependentSet: Set<Dependent<any>>
-}
-
-export type Dependent<T> = State<T> & {
-	update: (Dependent<T>) -> boolean,
-	dependencySet: Set<Dependency<any>>
-}
-
+-- Types that can be expressed as vectors of numbers, and so can be animated.
 export type Animatable =
 	number |
 	CFrame |
@@ -62,22 +44,69 @@ export type Animatable =
 	Vector3 |
 	Vector3int16
 
-export type Tween<T> = {
-	get: (Tween<T>, asDependency: boolean?) -> State<T>,
-	update: (Tween<T>) -> (),
-	-- Uncomment when ENABLE_PARAM_SETTERS is enabled
-	-- setTweenInfo: (Tween<T>, newTweenInfo: TweenInfo) -> ()
+--[[
+	Generic reactive graph types
+]]
+
+-- A graph object which can have dependents.
+export type Dependency = {
+	dependentSet: Set<Dependent>
 }
 
-export type Spring<T> = {
-	get: (Spring<T>, asDependency: boolean?) -> any,
-	update: (Spring<T>) -> (),
+-- A graph object which can have dependencies.
+export type Dependent = {
+	update: (Dependent) -> boolean,
+	dependencySet: Set<Dependency>
+}
+
+-- An object which stores a piece of reactive state.
+export type StateObject<T> = Dependency & {
+	type: string, -- replace with "State" when Luau supports singleton types
+	kind: string,
+	get: (StateObject<T>, asDependency: boolean?) -> T
+}
+
+-- Either a constant value of type T, or a state object containing type T.
+export type StateOrValue<T> = StateObject<T> | T
+
+--[[
+	Specific reactive graph types
+]]
+
+-- A state object whose value can be set at any time by the user.
+export type State<T> = StateObject<T> & {
+	-- kind: "State" (add this when Luau supports singleton types)
+ 	set: (State<T>, newValue: any, force: boolean?) -> ()
+}
+
+-- A state object whose value is derived from other objects using a callback.
+export type Computed<T> = StateObject<T> & Dependent & {
+	-- kind: "Computed" (add this when Luau supports singleton types)
+}
+
+-- A state object whose value is derived from other objects using a callback.
+export type ComputedPairs<K, V> = StateObject<{[K]: V}> & Dependent & {
+	-- kind: "ComputedPairs" (add this when Luau supports singleton types)
+}
+
+-- A state object which follows another state object using tweens.
+export type Tween<T> = StateObject<T> & Dependent & {
+	-- kind: "Tween" (add this when Luau supports singleton types)
+}
+
+-- A state object which follows another state object using spring simulation.
+export type Spring<T> = StateObject<T> & Dependent & {
+	-- kind: "Spring" (add this when Luau supports singleton types)
 	-- Uncomment when ENABLE_PARAM_SETTERS is enabled
-	-- setDamping: (Spring<T>, damping: number) -> (),
-	-- setSpeed: (Spring<T>, speed: number) -> (),
 	-- setPosition: (Spring<T>, newValue: Animatable) -> (),
 	-- setVelocity: (Spring<T>, newValue: Animatable) -> (),
 	-- addVelocity: (Spring<T>, deltaValue: Animatable) -> ()
+}
+
+-- An object which can listen for updates on another state object.
+export type Compat = Dependent & {
+	-- kind: "Compat" (add this when Luau supports singleton types)
+  	onChange: (Compat, callback: () -> ()) -> ()
 }
 
 return nil
