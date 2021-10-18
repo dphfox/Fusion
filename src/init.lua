9d59b2d89676035c0e0a510c591302fa6ee64e78
Elttob <dan.bottlefact@gmail.com>
2021-10-18 17:30:44 +0100

    Main module type checking

--- src/init.lua
+++ src/init.lua
@@ -1,3 +1,5 @@
+--!strict
+
 --[[
 	The entry point for the Fusion library.
 ]]
@@ -5,27 +7,29 @@
 local Types = require(script.Types)
 local restrictRead = require(script.Utility.restrictRead)
 
-export type State<T> = Types.State<T>
+export type StateObject<T> = Types.StateObject<T>
 export type StateOrValue<T> = Types.StateOrValue<T>
 export type Symbol = Types.Symbol
+export type State<T> = Types.State<T>
 export type Computed<T> = Types.Computed<T>
+export type ComputedPairs<K, V> = Types.ComputedPairs<K, V>
 export type Compat = Types.Compat
 export type Tween<T> = Types.Tween<T>
 export type Spring<T> = Types.Spring<T>
 
 type Fusion = {
-  New: (className: string) -> ((propertyTable: {[string | Types.Symbol]: any}) -> Instance | nil),
-  Children: Types.Symbol,
-  OnEvent: (eventName: string) -> Types.Symbol,
-  OnChange: (propertyName: string) -> Types.Symbol,
+  New: (className: string) -> ((propertyTable: Types.PropertyTable) -> Instance),
+  Children: Types.ChildrenKey,
+  OnEvent: (eventName: string) -> Types.OnEventKey,
+  OnChange: (propertyName: string) -> Types.OnChangeKey,
 
-  State: (initialValue: any) -> State<any>,
-  Computed: (callback: () -> any) -> Computed<any>,
-  ComputedPairs: (inputTable: Types.StateOrValue<{[any]: any}>, processor: (any) -> any, destructor: (any) -> ()?) -> Computed<any>,
-  Compat: (watchedState: Types.State<any>) -> Compat,
+  State: <T>(initialValue: T) -> State<T>,
+  Computed: <T>(callback: () -> T) -> Computed<T>,
+  ComputedPairs: <K, VI, VO>(inputTable: StateOrValue<{[K]: VI}>, processor: (K, VI) -> VO, destructor: (VO) -> ()?) -> ComputedPairs<K, VO>,
+  Compat: (watchedState: StateObject<any>) -> Compat,
 
-  Tween: (goalState: Types.State<Types.Animatable>, tweenInfo: TweenInfo?) -> Tween<any>,
-  Spring: (goalState: Types.State<Types.Animatable>, speed: number?, damping: number?) -> Spring<any>
+  Tween: <T>(goalState: StateObject<T>, tweenInfo: TweenInfo?) -> Tween<T>,
+  Spring: <T>(goalState: StateObject<T>, speed: number?, damping: number?) -> Spring<T>
 }
 
 return restrictRead("Fusion", {
