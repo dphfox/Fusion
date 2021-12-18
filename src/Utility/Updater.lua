247cb7e4f3e5de8efcb45e984717868e68d621b5
britton <brittonkfischer@gmail.com>
2021-12-18 10:26:28 -0700

    fix Updater issues
    as discussed in #98

--- src/Utility/Updater.lua
+++ src/Utility/Updater.lua
@@ -2,16 +2,11 @@
 
 --[[
     Centralizes all RunService connections so that the various parts of Fusion 
-    don't connect to RunService independently of one another.
+    don't connect to RunService independently of one another. This returns a
+    singleton, and therefore its functions all use lua object syntax with `:function()`.
 
     You can optionally pause all internal updating by calling `:pause()`, and
     then resume updating by calling `:resume()`.
-
-    If you want to manually control the update process, you can call
-    `.thread:Disconnect()`, since `.thread` is a Roblox Signal. After doing this, 
-    you then just need to account for throttling using the `.throttling` property,
-    and call `.stepped:Fire(dt)` whenever you want; `dt` here is just the change in 
-    time since the last update call.
 ]]
 
 local RunService = game:GetService("RunService")
@@ -19,34 +14,38 @@ local RunService = game:GetService("RunService")
 local Package = script.Parent.Parent
 local Signal = require(Package.Parent.Signal)
 
-local class = {}
-class.__index = class
+local singleton = {}
 
-function class.new()
-    local self = setmetatable({}, class)
+singleton._stepped = Signal.new
+singleton._paused = false
+singleton._thread = (RunService:IsServer() and RunService.Stepped or RunService.RenderStepped):Connect(function(dt)
+    if not singleton._paused then
+        singleton._stepped:Fire(dt)
+    end
+end)
 
-    self.stepped = Signal.new()
-    self.throttling = false
-    self.thread = (RunService:IsServer() and self.Stepped or self.RenderStepped):Connect(function(dt)
-        if not self.Throttling then
-            self.stepped:Fire(dt)
-        end
-    end)
+--[[
+    Binds a function to be called whenever the Updater gets stepped. By default, this function will
+    be called with `RunService.Stepped` on the server and `RunService.RenderStepped` on the client.
 
-    return self
+    For future-proofing reasons you should not directly call ``._stepped:Connect(func)``
+]]
+function singleton:bindToStep(func)
+    return singleton._stepped:Connect(func)
 end
 
-function class:bindToStep(func)
-    return self.Stepped:Connect(func)
+--[[
+    Pauses the execution of updates. Updates will resume as soon as you call `:resume()`
+]]
+function singleton:pause()
+    singleton._paused = true
 end
 
-function class:pause()
-    self.Throttling = true
+--[[
+    Resumes the execution of updates.
+]]
+function singleton:resume()
+    singleton._paused = false
 end
 
-function class:play()
-    self.Throttling = false
-end
-
--- Return a Singleton of the Updater class
-return class.new()
+return singleton
