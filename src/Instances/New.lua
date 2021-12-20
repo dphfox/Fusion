a6b817994cecce02c432a90555c23d805afdaf7e
Elttob <dan.bottlefact@gmail.com>
2021-12-20 17:30:18 +0000

    Figure out priorities

--- src/Instances/New.lua
+++ src/Instances/New.lua
@@ -10,6 +10,18 @@ local PubTypes = require(Package.PubTypes)
 
 local function New(className: string)
 	return function(propertyTable: PubTypes.PropertyTable): Instance
+
+		-- stage 1: configure self
+		--     properties
+		-- stage 2: configure descendants
+		--     Children
+		-- stage 3: configure ancestor
+		--     Parent
+		-- stage 4: configure observers
+		--     Ref
+		--     OnEvent / OnChange
+
+
 		-- TODO: implement this
 	end
 end
