01ed4a873276488342b0e1488d80517b6fca7efb
Elttob <dan.bottlefact@gmail.com>
2021-12-24 17:15:23 +0000

    Change applyInstanceProps_impl to use ref

--- src/Instances/applyInstanceProps.lua
+++ src/Instances/applyInstanceProps.lua
@@ -6,12 +6,20 @@
 
 	No strong reference is kept by default - special keys should take care not
 	to accidentally hold strong references to instances forever.
+
+	If a property of an instance is assigned to twice, an error will be raised.
+	It's encouraged for special keys to do the same where multiple assignment
+	doesn't make sense.
 ]]
 
 local Package = script.Parent.Parent
 local PubTypes = require(Package.PubTypes)
+local Types = require(Package.Types)
+local semiWeakRef = require(Package.Instances.semiWeakRef)
 
-local function applyInstanceProps(props: PubTypes.PropertyTable, applyTo: Instance)
+
+
+local function applyInstanceProps_impl(props: PubTypes.PropertyTable, applyToRef: Types.SemiWeakRef)
 	-- stage 1: configure self
 	--     properties
 	-- stage 2: configure descendants
@@ -23,6 +31,10 @@ local function applyInstanceProps(props: PubTypes.PropertyTable, applyTo: Instan
 	--     OnEvent / OnChange
 
 	-- TODO: implement this
+end
+
+local function applyInstanceProps(props: PubTypes.PropertyTable, applyTo: Instance)
+	applyInstanceProps_impl(props, semiWeakRef(applyTo))
 end
 
 return applyInstanceProps
