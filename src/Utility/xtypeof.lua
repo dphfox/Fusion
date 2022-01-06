317d6ae895a071f86d80879d15d2f7c9cee42cca
Elttob <dan.bottlefact@gmail.com>
2022-01-06 08:40:00 +0000

    xtypeof requires `type` to be string

--- src/Utility/xtypeof.lua
+++ src/Utility/xtypeof.lua
@@ -2,15 +2,15 @@
 
 --[[
 	Extended typeof, designed for identifying custom objects.
-	If given a table with a `type` field, returns that.
+	If given a table with a `type` string, returns that.
 	Otherwise, returns `typeof()` the argument.
 ]]
 
 local function xtypeof(x: any)
 	local typeString = typeof(x)
 
-	if typeString == "table" then
-		return x.type or typeString
+	if typeString == "table" and typeof(x.type) == "string" then
+		return x.type
 	else
 		return typeString
 	end
