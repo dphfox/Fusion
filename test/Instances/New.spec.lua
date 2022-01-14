3b54a71f700f5b53b9d53a2aac2b35f1442fcf10
Elttob <dan.bottlefact@gmail.com>
2022-01-14 15:45:48 +0000

    Extract [OnChange] and [Ref] unit tests

--- test/Instances/New.spec.lua
+++ test/Instances/New.spec.lua
@@ -128,56 +128,6 @@ return function()
 		end).to.throw("unrecognisedPropertyKey")
 	end)
 
-	it("should connect property change handlers", function()
-		local fires = 0
-		local ins = New "Folder" {
-			Name = "Foo",
-
-			[OnChange "Name"] = function()
-				fires += 1
-			end
-		}
-
-		ins.Name = "Bar"
-
-		waitForDefer()
-
-		expect(fires).never.to.equal(0)
-	end)
-
-	it("should throw when connecting to non-existent property changes", function()
-		expect(function()
-			New "Folder" {
-				Name = "Foo",
-
-				[OnChange "Frobulate"] = function() end
-			}
-		end).to.throw("cannotConnectChange")
-	end)
-
-	it("shouldn't fire property changes during initialisation", function()
-		local fires = 0
-		local ins = New "Folder" {
-			Parent = game,
-			Name = "Foo",
-
-			[OnChange "Name"] = function()
-				fires += 1
-			end,
-
-			[OnChange "Parent"] = function()
-				fires += 1
-			end
-		}
-
-		local totalFires = fires
-		ins:Destroy()
-
-		waitForDefer()
-
-		expect(totalFires).to.equal(0)
-	end)
-
 	it("should bind State objects passed as properties", function()
 		local name = Value("Foo")
 		local ins = New "Folder" {
@@ -223,16 +173,6 @@ return function()
 		expect(ins.Name).to.equal("Foo")
 		waitForDefer()
 		expect(ins.Name).to.equal("Baz")
-	end)
-
-	it("should set State objects passed as [Ref]", function()
-		local refValue = Value()
-
-		local child = New "Folder" {
-			[Ref] = refValue
-		}
-
-		expect(refValue:get()).to.equal(child)
 	end)
 
 	it("should bind State objects passed as the parent", function()
