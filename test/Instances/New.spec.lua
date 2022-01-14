f5eb0c63a4b62061493a828b8128ca2b2a957c95
Elttob <dan.bottlefact@gmail.com>
2022-01-14 15:37:27 +0000

    Move [Children] unit tests out

--- test/Instances/New.spec.lua
+++ test/Instances/New.spec.lua
@@ -2,10 +2,10 @@ local RunService = game:GetService("RunService")
 
 local Package = game:GetService("ReplicatedStorage").Fusion
 local New = require(Package.Instances.New)
-local Children = require(Package.Instances.Children)
 local Ref = require(Package.Instances.Ref)
 local OnEvent = require(Package.Instances.OnEvent)
 local OnChange = require(Package.Instances.OnChange)
+local Children = require(Package.Instances.Children)
 
 local Value = require(Package.State.Value)
 local Computed = require(Package.State.Computed)
@@ -126,66 +126,6 @@ return function()
 				}] = true
 			}
 		end).to.throw("unrecognisedPropertyKey")
-	end)
-
-	it("should assign single children to instances", function()
-		local ins = New "Folder" {
-			Name = "Bob",
-
-			[Children] = New "Folder" {
-				Name = "Fred"
-			}
-		}
-
-		expect(ins:FindFirstChild("Fred")).to.be.ok()
-	end)
-
-	it("should assign multiple children to instances", function()
-		local ins = New "Folder" {
-			Name = "Bob",
-
-			[Children] = {
-				New "Folder" {
-					Name = "Fred"
-				},
-				New "Folder" {
-					Name = "George"
-				},
-				New "Folder" {
-					Name = "Harry"
-				}
-			}
-		}
-
-		expect(ins:FindFirstChild("Fred")).to.be.ok()
-		expect(ins:FindFirstChild("George")).to.be.ok()
-		expect(ins:FindFirstChild("Harry")).to.be.ok()
-	end)
-
-	it("should flatten children to be assigned", function()
-		local ins = New "Folder" {
-			Name = "Bob",
-
-			[Children] = {
-				New "Folder" {
-					Name = "Fred"
-				},
-				{
-					New "Folder" {
-						Name = "George"
-					},
-					{
-						New "Folder" {
-							Name = "Harry"
-						}
-					}
-				}
-			}
-		}
-
-		expect(ins:FindFirstChild("Fred")).to.be.ok()
-		expect(ins:FindFirstChild("George")).to.be.ok()
-		expect(ins:FindFirstChild("Harry")).to.be.ok()
 	end)
 
 	it("should connect event handlers", function()
@@ -354,18 +294,6 @@ return function()
 		expect(ins.Name).to.equal("Baz")
 	end)
 
-	itSKIP("should respect Parents of assigned children", function()
-		local targetParent = New "Folder" {}
-		local child = New "Folder" {
-			Parent = targetParent
-		}
-		local constructedParent = New "Folder" {
-			[Children] = child
-		}
-
-		expect(child.Parent).to.equal(targetParent)
-	end)
-
 	it("should set State objects passed as [Ref]", function()
 		local refValue = Value()
 
@@ -395,118 +323,4 @@ return function()
 
 		expect(child.Parent).to.equal(parent2)
 	end)
-
-	it("should bind State objects passed as children", function()
-		local child1 = New "Folder" {}
-		local child2 = New "Folder" {}
-		local child3 = New "Folder" {}
-		local child4 = New "Folder" {}
-
-		local children = Value({child1})
-
-		local parent = New "Folder" {
-			[Children] = {
-				children
-			}
-		}
-
-		expect(child1.Parent).to.equal(parent)
-
-		children:set({child2, child3})
-
-		waitForDefer()
-
-		expect(child1.Parent).to.equal(nil)
-		expect(child2.Parent).to.equal(parent)
-		expect(child3.Parent).to.equal(parent)
-
-		children:set({child1, child2, child3, child4})
-
-		waitForDefer()
-
-		expect(child1.Parent).to.equal(parent)
-		expect(child2.Parent).to.equal(parent)
-		expect(child3.Parent).to.equal(parent)
-		expect(child4.Parent).to.equal(parent)
-	end)
-
-	it("should defer updates to State children", function()
-		local child1 = New "Folder" {}
-		local child2 = New "Folder" {}
-
-		local children = Value(child1)
-
-		local parent = New "Folder" {
-			[Children] = {
-				children
-			}
-		}
-
-		expect(child1.Parent).to.equal(parent)
-
-		children:set(child2)
-
-		expect(child1.Parent).to.equal(parent)
-		expect(child2.Parent).to.equal(nil)
-
-		waitForDefer()
-
-		expect(child1.Parent).to.equal(nil)
-		expect(child2.Parent).to.equal(parent)
-	end)
-
-	it("should recursively bind State children", function()
-		local child1 = New "Folder" {}
-		local child2 = New "Folder" {}
-		local child3 = New "Folder" {}
-		local child4 = New "Folder" {}
-
-		local children = Value({
-			child1,
-			Value(child2),
-			Value({
-				child3,
-				Value(Value(child4))
-			})
-		})
-
-		local parent = New "Folder" {
-			[Children] = {
-				children
-			}
-		}
-
-		expect(child1.Parent).to.equal(parent)
-		expect(child2.Parent).to.equal(parent)
-		expect(child3.Parent).to.equal(parent)
-		expect(child4.Parent).to.equal(parent)
-	end)
-
-	it("should allow for State children to be nil", function()
-		local child = New "Folder" {}
-
-		local children = Value(nil)
-
-		local parent = New "Folder" {
-			[Children] = {
-				children
-			}
-		}
-
-		expect(child.Parent).to.equal(nil)
-
-		children:set(child)
-
-		waitForDefer()
-
-		expect(child.Parent).to.equal(parent)
-
-		children:set(nil)
-
-		waitForDefer()
-
-		expect(child.Parent).to.equal(nil)
-	end)
-
-	-- TODO: test for garbage collection
 end
