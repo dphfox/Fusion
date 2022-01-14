94a5b437378452b728d04e0c3d7fd46b69a24f97
Elttob <dan.bottlefact@gmail.com>
2022-01-14 16:39:57 +0000

    Rework New/applyInstanceProps unit tests

--- test/Instances/New.spec.lua
+++ test/Instances/New.spec.lua
@@ -2,18 +2,7 @@ local RunService = game:GetService("RunService")
 
 local Package = game:GetService("ReplicatedStorage").Fusion
 local New = require(Package.Instances.New)
-local Ref = require(Package.Instances.Ref)
-local OnEvent = require(Package.Instances.OnEvent)
-local OnChange = require(Package.Instances.OnChange)
-local Children = require(Package.Instances.Children)
-
-local Value = require(Package.State.Value)
-local Computed = require(Package.State.Computed)
-
-local function waitForDefer()
-	RunService.RenderStepped:Wait()
-	RunService.RenderStepped:Wait()
-end
+local defaultProps = require(Package.Instances.defaultProps)
 
 return function()
 	it("should create a new instance", function()
@@ -29,169 +18,13 @@ return function()
 		end).to.throw("cannotCreateClass")
 	end)
 
-	it("should assign properties to instances", function()
-		local ins = New "Folder" {
-			Name = "Bob"
-		}
+	it("should apply 'sensible default' properties", function()
+		for className, defaults in pairs(defaultProps) do
+			local ins = New (className) {}
 
-		expect(ins.Name).to.equal("Bob")
-	end)
-
-	it("should throw for non-existent constant properties", function()
-		expect(function()
-			New "Folder" {
-				Frobulator = "Frobulateur"
-			}
-		end).to.throw("cannotAssignProperty")
-	end)
-
-	it("should throw for non-existent value properties", function()
-		local state = Value("Frobulateur")
-
-		expect(function()
-			New "Folder" {
-				Frobulator = Computed(function()
-					state:get()
-				end)
-			}
-		end).to.throw("cannotAssignProperty")
-	end)
-
-	it("should throw on invalid property type for non-Parent", function()
-		expect(function()
-			New "Folder" {
-				Name = UDim.new()
-			}
-		end).to.throw("invalidPropertyType")
-
-		local state = Value(true)
-
-		expect(function()
-			New "Folder" {
-				Name = Computed(function()
-					state:get()
-				end)
-			}
-		end).to.throw("invalidPropertyType")
-	end)
-
-	it("should throw on invalid property type for Parent", function()
-		expect(function()
-			New "Folder" {
-				Parent = "Foo"
-			}
-		end).to.throw("invalidPropertyType")
-
-		local state = Value(true)
-
-		expect(function()
-			New "Folder" {
-				Parent = Computed(function()
-					return state:get()
-				end)
-			}
-		end).to.throw("invalidPropertyType")
-	end)
-
-	it("should throw on invalid property type for Instances", function()
-		expect(function()
-			New "ObjectValue" {
-				Value = "Foo"
-			}
-		end).to.throw("invalidPropertyType")
-
-		local state = Value(true)
-
-		expect(function()
-			New "ObjectValue" {
-				Value = Computed(function()
-					return state:get()
-				end)
-			}
-		end).to.throw("invalidPropertyType")
-	end)
-
-	it("should throw for unrecognised keys", function()
-		expect(function()
-			New "Folder" {
-				[2] = true
-			}
-		end).to.throw("unrecognisedPropertyKey")
-
-		expect(function()
-			New "Folder" {
-				[{
-					type = "Symbol",
-					name = "Fake"
-				}] = true
-			}
-		end).to.throw("unrecognisedPropertyKey")
-	end)
-
-	it("should bind State objects passed as properties", function()
-		local name = Value("Foo")
-		local ins = New "Folder" {
-			Name = name
-		}
-
-		expect(ins.Name).to.equal("Foo")
-
-		name:set("Bar")
-		waitForDefer()
-		expect(ins.Name).to.equal("Bar")
-	end)
-
-	it("should bind Computed objects passed as properties", function()
-		local name = Value("Foo")
-		local ins = New "Folder" {
-			Name = Computed(function()
-				return "The" .. name:get()
-			end)
-		}
-
-		expect(ins.Name).to.equal("TheFoo")
-
-		name:set("Bar")
-		waitForDefer()
-		expect(ins.Name).to.equal("TheBar")
-	end)
-
-	it("should defer bound state updates", function()
-		local name = Value("Foo")
-		local ins = New "Folder" {
-			Name = name
-		}
-
-		expect(ins.Name).to.equal("Foo")
-
-		name:set("Bar")
-
-		expect(ins.Name).to.equal("Foo")
-
-		name:set("Baz")
-
-		expect(ins.Name).to.equal("Foo")
-		waitForDefer()
-		expect(ins.Name).to.equal("Baz")
-	end)
-
-	it("should bind State objects passed as the parent", function()
-		local parent1 = New "Folder" {}
-		local parent2 = New "Folder" {}
-
-		local parent = Value(parent1)
-
-		local child = New "Folder" {
-			Parent = parent
-		}
-
-		expect(child.Parent).to.equal(parent1)
-
-		parent:set(parent2)
-		expect(child.Parent).to.equal(parent1)
-
-		waitForDefer()
-
-		expect(child.Parent).to.equal(parent2)
+			for propName, propValue in pairs(defaults) do
+				expect(ins[propName]).to.equal(propValue)
+			end
+		end
 	end)
 end
