1ce4e502416ec636ac9b16d6a5c01cf5c32c26f8
Elttob <dan.bottlefact@gmail.com>
2022-02-04 04:45:59 +0000

    Don't forget to initialise out props

--- src/Instances/Out.lua
+++ src/Instances/Out.lua
@@ -24,6 +24,7 @@ local function Out(propertyName: string): PubTypes.SpecialKey
 		elseif xtypeof(outState) ~= "State" or outState.kind ~= "Value" then
 				logError("invalidOutType")
 		else
+			outState:set((applyToRef.instance :: any)[propertyName])
 			table.insert(cleanupTasks, event:Connect(function()
 				if applyToRef.instance ~= nil then
 					outState:set((applyToRef.instance :: any)[propertyName])
