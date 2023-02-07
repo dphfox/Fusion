9a600fc03ed5cdcca33ae50c297ed0f583594e40
Krypt <kryptonforce102@gmail.com>
2023-02-08 10:04:13 +1100

    Add state support for OnEvent

--- src/Instances/OnEvent.lua
+++ src/Instances/OnEvent.lua
@@ -8,6 +8,7 @@
 local Package = script.Parent.Parent
 local PubTypes = require(Package.PubTypes)
 local logError = require(Package.Logging.logError)
+local xtypeof = require(Package.Utility.xtypeof)
 
 local function getProperty_unsafe(instance: Instance, property: string)
 	return (instance :: any)[property]
@@ -23,10 +24,18 @@ local function OnEvent(eventName: string): PubTypes.SpecialKey
 		local ok, event = pcall(getProperty_unsafe, applyTo, eventName)
 		if not ok or typeof(event) ~= "RBXScriptSignal" then
 			logError("cannotConnectEvent", nil, applyTo.ClassName, eventName)
-		elseif typeof(callback) ~= "function" then
+			return
+		end
+
+		if typeof(callback) ~= "function" and xtypeof(callback) ~= "state" then
 			logError("invalidEventHandler", nil, eventName)
 		else
-			table.insert(cleanupTasks, event:Connect(callback))
+			table.insert(cleanupTasks, event:Connect(function()
+				if xtypeof(callback) == "state" then
+					callback = peek(callback)
+				end
+				callback()
+			end))
 		end
 	end
 
