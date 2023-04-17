bcd9ee20227f7bc2f90dc64b224639124d8bd4ec
Elttob <dan.bottlefact@gmail.com>
2023-04-17 14:46:13 +0100

    Updated displacement space utils

--- src/Animation/spaces.lua
+++ src/Animation/spaces.lua
@@ -4,7 +4,38 @@
 
 -- TODO: type annotate this file
 
-local linear = {
+local spaces = {}
+
+spaces.makeCompositeSpace = function(ranges)
+	local dimension = 0
+	for _, data in ranges do
+		dimension = math.max(dimension, data.to)
+	end
+	return {
+		toDisplacement = function(position, relativeTo)
+			local displacement = {}
+			for _, rangeInfo in ranges do
+				local subPosition = table.move(position, rangeInfo.from, rangeInfo.to, 1, {})
+				local subRelativeTo = table.move(relativeTo, rangeInfo.from, rangeInfo.to, 1, {})
+				local subDisplacement = rangeInfo.space.toDisplacement(subPosition, subRelativeTo)
+				table.move(subDisplacement, 1, rangeInfo.to - rangeInfo.from + 1, rangeInfo.from, displacement)
+			end
+			return displacement
+		end,
+		toPosition = function(displacement, relativeTo)
+			local position = {}
+			for _, rangeInfo in ranges do
+				local subDisplacement = table.move(displacement, rangeInfo.from, rangeInfo.to, 1, {})
+				local subRelativeTo = table.move(relativeTo, rangeInfo.from, rangeInfo.to, 1, {})
+				local subPosition = rangeInfo.space.toPosition(subDisplacement, subRelativeTo)
+				table.move(subPosition, 1, rangeInfo.to - rangeInfo.from + 1, rangeInfo.from, position)
+			end
+			return position
+		end
+	}
+end
+
+spaces.linear = {
 	toDisplacement = function(position, relativeTo)
 		local displacement = table.create(#position)
 		for index, component in position do
@@ -21,7 +52,7 @@ local linear = {
 	end
 }
 
-local function linearWrapped(minimum, maximum)
+spaces.linearWrapped = function(minimum, maximum)
 	assert(minimum <= maximum, "Wrapped space requires minimum value <= maximum value")
 	local range = maximum - minimum
 	local halfRange = range / 2
@@ -43,11 +74,26 @@ local function linearWrapped(minimum, maximum)
 	}
 end
 
-return {
-	linear = linear,
-	linearWrapped = linearWrapped,
-	degrees = linearWrapped(0, 360),
-	signedDegrees = linearWrapped(-180, 180),
-	radians = linearWrapped(0, 2 * math.pi),
-	signedRadians = linearWrapped(-math.pi, math.pi)
+spaces.degrees = spaces.linearWrapped(0, 360)
+spaces.signedDegrees = spaces.linearWrapped(-180, 180)
+spaces.radians = spaces.linearWrapped(0, 2 * math.pi)
+spaces.signedRadians = spaces.linearWrapped(-math.pi, math.pi)
+
+spaces.quaternion = {
+	toDisplacement = function(position, relativeTo)
+		local displacement = table.create(#position)
+		for index, component in position do
+			displacement[index] = component -- TODO: how do you do this?
+		end
+		return displacement
+	end,
+	toPosition = function(displacement, relativeTo)
+		local position = table.create(#displacement)
+		for index, component in displacement do
+			position[index] = component -- TODO: how do you do this?
+		end
+		return position
+	end
 }
+
+return spaces
