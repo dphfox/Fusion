0b8d7a2cd1096993e7af17a25aced5ccaf899ae2
Elttob <dan.bottlefact@gmail.com>
2023-09-02 04:27:57 +0100

    Adjust return type of scoped()

--- src/Memory/scoped.lua
+++ src/Memory/scoped.lua
@@ -4,9 +4,12 @@
 	Creates cleanup tables with access to constructors as methods.
 ]]
 
+local Package = script.Parent.Parent
+local PubTypes = require(Package.PubTypes)
+
 -- This return type is technically a lie, but it's required for useful type
 -- checking behaviour.
-local function scoped<T>(constructors: T): T
+local function scoped<T>(constructors: T): {PubTypes.Task} & T
 	return setmetatable({}, {__index = constructors}) :: any
 end
 
