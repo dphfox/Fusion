7242de7e934553318d8a226cebaf49e6299bd3c1
Elttob <dan.bottlefact@gmail.com>
2023-09-03 07:30:50 +0100

    Observer unit tests

--- test/State/Observer.spec.lua
+++ test/State/Observer.spec.lua
@@ -1,66 +1,112 @@
-local RunService = game:GetService("RunService")
-
 local Package = game:GetService("ReplicatedStorage").Fusion
 local Observer = require(Package.State.Observer)
 local Value = require(Package.State.Value)
 local peek = require(Package.State.peek)
+local doCleanup = require(Package.Memory.doCleanup)
 
 return function()
-	it("should fire connections on change", function()
-		local state = Value(false)
-		local observer = Observer(state)
+	it("constructs in scopes", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer(scope, dependency)
 
-		local changed = false
-		observer:onChange(function()
-			changed = true
-		end)
+		expect(observer).to.be.a("table")
+		expect(observer.type).to.equal("State")
+		expect(observer.kind).to.equal("Observer")
+		expect(scope[2]).to.equal(observer)
 
-		state:set(true)
-
-		-- Wait twice in case it gets deferred
-		RunService.RenderStepped:Wait()
-		RunService.RenderStepped:Wait()
-
-		expect(changed).to.equal(true)
+		doCleanup(scope)
 	end)
 
-	it("should fire connections only after the value changes", function()
-		local state = Value(false)
-		local observer = Observer(state)
-
-		local changedValue
-		local completed = false
-		observer:onChange(function(value)
-			changedValue = peek(state)
-			completed = true
-		end)
-
-		state:set(true)
-
-		-- Wait twice in case it gets deferred
-		RunService.RenderStepped:Wait()
-		RunService.RenderStepped:Wait()
-
-		expect(changedValue).to.equal(true)
+	it("is destroyable", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer(scope, dependency)
+		expect(function()
+			observer:destroy()
+		end).to.never.throw()
 	end)
 
-	it("should fire connections only once after the value changes", function()
-		local state = Value(false)
-		local observer = Observer(state)
-
-		local timesFired = 0
-		local completed = false
-		observer:onChange(function(value)
-			timesFired += 1
-			completed = true
+	it("fires once after change", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer(scope, dependency)
+		local numFires = 0
+		local disconnect = observer:onChange(function()
+			numFires += 1
 		end)
+		dependency:set(15)
+		disconnect()
 
-		state:set(true)
+		expect(numFires).to.equal(1)
 
-		-- Wait twice in case it gets deferred
-		RunService.RenderStepped:Wait()
-		RunService.RenderStepped:Wait()
-
-		expect(timesFired).to.equal(1)
+		doCleanup(scope)
 	end)
+
+	it("fires asynchronously", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer(scope, dependency)
+		local numFires = 0
+		local disconnect = observer:onChange(function()
+			task.wait(1)
+			numFires += 1
+		end)
+		dependency:set(15)
+		disconnect()
+
+		expect(numFires).to.equal(0)
+
+		doCleanup(scope)
+	end)
+
+	it("fires onBind at bind time", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer(scope, dependency)
+		local numFires = 0
+		local disconnect = observer:onBind(function()
+			numFires += 1
+		end)
+		disconnect()
+
+		expect(numFires).to.equal(1)
+
+		doCleanup(scope)
+	end)
+
+	it("disconnects properly", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer(scope, dependency)
+		local numFires = 0
+		local disconnect = observer:onChange(function()
+			numFires += 1
+		end)
+		dependency:set(15)
+		disconnect()
+		dependency:set(2)
+
+		expect(numFires).to.equal(1)
+
+		doCleanup(scope)
+	end)
+
+	it("disconnects on destroy", function()
+		local scope = {}
+		local dependency = Value(scope, 5)
+		local observer = Observer({}, dependency)
+		local numFires = 0
+		local _ = observer:onChange(function()
+			numFires += 1
+		end)
+		dependency:set(15)
+		observer:destroy()
+		dependency:set(2)
+
+		expect(numFires).to.equal(1)
+
+		doCleanup(scope)
+	end)
+
 end
