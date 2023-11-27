fa4ea0494c66a8152682fcb022f5a16031fd2fe6
Elttob <dan.bottlefact@gmail.com>
2023-11-27 13:51:00 +0000

    Output table construction

--- src/State/For.lua
+++ src/State/For.lua
@@ -36,123 +36,142 @@ type Processor = {
 ]]
 
 function class:update(): boolean
-	local newInputTable = peek(self._inputTable)
-	local existingProcessors = self._existingProcessors :: {[Processor]: true}
-
+	local existingInputTable = self._existingInputTable
+	local existingOutputTable = self._existingOutputTable
+	local existingProcessors = self._existingProcessors
+	local newInputTable = peek(self._newInputTable)
+	local newOutputTable = self._newOutputTable
+	local newProcessors = self._newProcessors
 	local remainingPairs = self._remainingPairs
-	table.clear(remainingPairs)
-	for key, value in newInputTable do
-		if remainingPairs[key] == nil then
-			remainingPairs[key] = {[value] = true}
-		else
-			remainingPairs[key][value] = true
-		end
+
+	-- clean out main dependency set
+	for dependency in pairs(self.dependencySet) do
+		dependency.dependentSet[self] = nil
+	end
+	table.clear(self.dependencySet)
+
+	if isState(self._inputTable) then
+		self._inputTable.dependentSet[self], self.dependencySet[self._inputTable] = true, true
 	end
 
-	local newProcessors: {[Processor]: true} = self._newProcessors
-	-- First, try and reuse processors who match both the key and value of a
-	-- remaining pair. This can be done with no recomputation.
-	for tryReuseProcessor in existingProcessors do
-		local key = peek(tryReuseProcessor.inputKey)
-		local value = peek(tryReuseProcessor.inputValue)
-		local remainingValues = remainingPairs[key]
-		if remainingValues ~= nil and remainingValues[value] ~= nil then
-			remainingValues[value] = nil
-			error("TODO: bring forward key from old table")
-			newProcessors[tryReuseProcessor] = true
-			existingProcessors[tryReuseProcessor] = nil
-		end
-	end
-	-- Next, try and reuse processors who match the key of a remaining pair.
-	-- The value will change but the key will stay stable.
-	for tryReuseProcessor in existingProcessors do
-		local key = peek(tryReuseProcessor.inputKey)
-		local remainingValues = remainingPairs[key]
-		if remainingValues ~= nil then
-			local value = next(remainingValues)
-			if value ~= nil then
-				remainingValues[value] = nil
-				tryReuseProcessor.inputValue:set(value)
-				error("TODO: bring forward key from old table")
-				newProcessors[tryReuseProcessor] = true
-				existingProcessors[tryReuseProcessor] = nil
-			end
-		end
-	end
-	-- Next, try and reuse processors who match the value of a remaining pair.
-	-- The key will change but the value will stay stable.
-	for tryReuseProcessor in existingProcessors do
-		local value = peek(tryReuseProcessor.inputValue)
-		for key, remainingValues in remainingPairs do
-			if remainingValues[value] ~= nil then
-				remainingValues[value] = nil
-				tryReuseProcessor.inputKey:set(key)
-				error("TODO: bring forward key from old table")
-				newProcessors[tryReuseProcessor] = true
-				existingProcessors[tryReuseProcessor] = nil
-			end
-		end
-	end
-	-- Finally, try and reuse any remaining processors, even if they do not
-	-- match a pair. Both key and value will be changed.
-	for tryReuseProcessor in existingProcessors do
-		for _, remainingValues in remainingPairs do
-			local value = next(remainingValues)
-			if value ~= nil then
-				remainingValues[value] = nil
-				tryReuseProcessor.inputValue:set(value)
-				error("TODO: bring forward key from old table")
-				newProcessors[tryReuseProcessor] = true
-				existingProcessors[tryReuseProcessor] = nil
-			end
-		end
-	end
-	-- By this point, we can be in one of three cases:
-	-- 1) some existing processors are left over; no remaining pairs (shrunk)
-	-- 2) no existing processors are left over; no remaining pairs (same size)
-	-- 3) no existing processors are left over; some remaining pairs (grew)
-	-- So, existing processors should be destroyed, and remaining pairs should
-	-- be created. This accomodates for table growth and shrinking.
-	for unusedProcessor in existingProcessors do
-		doCleanup(unusedProcessor.cleanupTask)
-	end
-	table.clear(existingProcessors)
-
-	for key, remainingValues in remainingPairs do
-		for value in remainingValues do
-			local scope = {}
-			local inputKey = Value(scope, key)
-			local inputValue = Value(scope, value)
-			local processOK, outputKey, outputValue = xpcall(self._processor, parseError, inputKey, inputValue)
-			if processOK then
-				local processor = {
-					inputKey = inputKey,
-					inputValue = inputValue,
-					outputKey = outputKey,
-					outputValue = outputValue,
-					cleanupTask = scope
-				}
-				newProcessors[processor] = true
+	if newInputTable ~= existingInputTable then
+		for key, value in newInputTable do
+			if remainingPairs[key] == nil then
+				remainingPairs[key] = {[value] = true}
 			else
-				logErrorNonFatal("forProcessorError", outputKey)
+				remainingPairs[key][value] = true
 			end
-			
 		end
+
+		-- First, try and reuse processors who match both the key and value of a
+		-- remaining pair. This can be done with no recomputation.
+		for tryReuseProcessor in existingProcessors do
+			local key = peek(tryReuseProcessor.inputKey)
+			local value = peek(tryReuseProcessor.inputValue)
+			local remainingValues = remainingPairs[key]
+			if remainingValues ~= nil and remainingValues[value] ~= nil then
+				remainingValues[value] = nil
+				newProcessors[tryReuseProcessor] = true
+				existingProcessors[tryReuseProcessor] = nil
+			end
+		end
+		-- Next, try and reuse processors who match the key of a remaining pair.
+		-- The value will change but the key will stay stable.
+		for tryReuseProcessor in existingProcessors do
+			local key = peek(tryReuseProcessor.inputKey)
+			local remainingValues = remainingPairs[key]
+			if remainingValues ~= nil then
+				local value = next(remainingValues)
+				if value ~= nil then
+					remainingValues[value] = nil
+					tryReuseProcessor.inputValue:set(value)
+					newProcessors[tryReuseProcessor] = true
+					existingProcessors[tryReuseProcessor] = nil
+				end
+			end
+		end
+		-- Next, try and reuse processors who match the value of a remaining pair.
+		-- The key will change but the value will stay stable.
+		for tryReuseProcessor in existingProcessors do
+			local value = peek(tryReuseProcessor.inputValue)
+			for key, remainingValues in remainingPairs do
+				if remainingValues[value] ~= nil then
+					remainingValues[value] = nil
+					tryReuseProcessor.inputKey:set(key)
+					newProcessors[tryReuseProcessor] = true
+					existingProcessors[tryReuseProcessor] = nil
+				end
+			end
+		end
+		-- Finally, try and reuse any remaining processors, even if they do not
+		-- match a pair. Both key and value will be changed.
+		for tryReuseProcessor in existingProcessors do
+			for _, remainingValues in remainingPairs do
+				local value = next(remainingValues)
+				if value ~= nil then
+					remainingValues[value] = nil
+					tryReuseProcessor.inputValue:set(value)
+					newProcessors[tryReuseProcessor] = true
+					existingProcessors[tryReuseProcessor] = nil
+				end
+			end
+		end
+		-- By this point, we can be in one of three cases:
+		-- 1) some existing processors are left over; no remaining pairs (shrunk)
+		-- 2) no existing processors are left over; no remaining pairs (same size)
+		-- 3) no existing processors are left over; some remaining pairs (grew)
+		-- So, existing processors should be destroyed, and remaining pairs should
+		-- be created. This accomodates for table growth and shrinking.
+		for unusedProcessor in existingProcessors do
+			doCleanup(unusedProcessor.cleanupTask)
+		end
+		
+		for key, remainingValues in remainingPairs do
+			for value in remainingValues do
+				local scope = {}
+				local inputKey = Value(scope, key)
+				local inputValue = Value(scope, value)
+				local processOK, outputKey, outputValue = xpcall(self._processor, parseError, inputKey, inputValue)
+				if processOK then
+					local processor = {
+						inputKey = inputKey,
+						inputValue = inputValue,
+						outputKey = outputKey,
+						outputValue = outputValue,
+						cleanupTask = scope
+					}
+					newProcessors[processor] = true
+				else
+					logErrorNonFatal("forProcessorError", outputKey)
+				end
+			end
+		end
+	end
+
+	for processor in newProcessors do
+		local key = peek(processor.outputKey)
+		local value = peek(processor.outputValue)
+		key.dependentSet[self], self.dependencySet[key] = true, true
+		value.dependentSet[self], self.dependencySet[value] = true, true
+		newOutputTable[processor.outputKey] = processor.outputValue
 	end
 
 	self._existingProcessors = newProcessors
+	self._existingOutputTable = newOutputTable
+	table.clear(existingOutputTable)
+	table.clear(existingProcessors)
+	table.clear(remainingPairs)
 	self._newProcessors = existingProcessors
+	self._newOutputTable = existingOutputTable
 
-	error("TODO: reconstruct table")
-
-	return error("TODO")
+	return true
 end
 
 --[[
 	Returns the interior value of this state object.
 ]]
 function class:_peek(): any
-	return self._outputTable
+	return self._existingOutputTable
 end
 
 function class:get()
@@ -185,17 +204,15 @@ local function For<KI, VI, KO, VO>(
 		dependentSet = setmetatable({}, WEAK_KEYS_METATABLE),
 		_processor = processor,
 		_inputTable = inputTable,
-		_outputTable = {},
+		_existingInputTable = nil,
+		_existingOutputTable = {},
 		_existingProcessors = {},
+		_newOutputTable = {},
 		_newProcessors = {},
 		_remainingPairs = {}
 	}, CLASS_METATABLE)
 
 	self:update()
-	if isState(self._inputTable) then
-		self._inputTable.dependentSet[self] = true
-		self.dependencySet[self._inputTable] = true
-	end
 	
 	table.insert(cleanupTable, self)
 
