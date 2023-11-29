1fd380e9dd6bef038eec1169cb2e87efd2fec341
Elttob <dan.bottlefact@gmail.com>
2023-11-29 11:43:30 +0000

    Rename to assertLifetime

--- src/Memory/assertLifetime.lua
+++ src/Memory/assertLifetime.lua
@@ -2,27 +2,27 @@
 
 --[[
 	Checks the scope of the parent to see if the child will be destroyed before
-	the parent is destroyed.
+	the parent is destroyed. If the parent is omitted, the scope will only be
+	checked for presence of the child.
 ]]
 local Package = script.Parent.Parent
 local PubTypes = require(Package.PubTypes)
-local logWarn = require(Package.Logging.logWarn)
 
-local function childAppearsLaterImpl(
+local function assertLifetimeImpl(
 	haystack: {any},
 	alreadyChecked: {[any]: true},
-	parent: any,
+	parent: any?,
 	child: any
 ): boolean?
 	for index = #haystack, 1, -1 do
 		local value = haystack[index]
-		if value == parent then
+		if parent ~= nil and value == parent then
 			return false
 		elseif value == child then
 			return true
 		elseif typeof(value) == "table" and value[1] ~= nil and alreadyChecked[value] == nil then
 			alreadyChecked[value] = true
-			local appearsLater = childAppearsLaterImpl(value, alreadyChecked, parent, child)
+			local appearsLater = assertLifetimeImpl(value, alreadyChecked, parent, child)
 			if appearsLater ~= nil then
 				return appearsLater
 			end
@@ -31,12 +31,12 @@ local function childAppearsLaterImpl(
 	return nil
 end
 
-local function childAppearsLater(
+local function assertLifetime(
 	scope: PubTypes.Scope<any>,
-	parent: any,
+	parent: any?,
 	child: any
 ): boolean
-	return childAppearsLaterImpl(scope, {}, parent, child) == true
+	return assertLifetimeImpl(scope, {}, parent, child) == true
 end
 
-return childAppearsLater
+return assertLifetime
